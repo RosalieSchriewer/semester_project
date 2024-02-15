82f2f7c9f887de5ff0ef20ad71ba2053840c61db
Rosalie <rosalie.schriewer@icloud.com>
2024-02-15 13:36:35 +0100

    can update and delete on DB

--- modules/routes/userRoute.mjs
+++ modules/routes/userRoute.mjs
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { raw } from "express";
 import User from "../user.mjs";
 import HTTPCodes from "../httpConstants.mjs";
 import jwt from 'jsonwebtoken';
 import { verifyToken } from "../authentication.mjs";
-import storageManager from "../storageManager.mjs";
+import  DBManager from "../storageManager.mjs";
 
 const USER_API = express.Router();
 
@@ -12,12 +12,12 @@ const users = []
 
 //url: localhost:8080/user -->lists all users
 /* 
-USER_API.get("/", (req, res) => {
+USER_API.get("/user", (req, res) => {
   res.status(HTTPCodes.SuccessfulResponse.Ok).json(users);
 }); 
  */
 //url: localhost:8080/user/id -->lists specific user
-
+/*
 USER_API.get("/:id", verifyToken, (req, res) => { 
     const userId = parseInt(req.params.id, 10); //has to be parsed for some reason, didn't work if i didn't
 
@@ -32,7 +32,7 @@ USER_API.get("/:id", verifyToken, (req, res) => {
     } else {
       res.status(HTTPCodes.ClientSideErrorResponse.BadRequest).send("Invalid user ID").end();
     }
-  });
+  });*/
 
 /*   -----------NEW USER--------------- */
 USER_API.post("/",async (req, res, next) => {
@@ -42,7 +42,7 @@ USER_API.post("/",async (req, res, next) => {
   if (name != "" && email != "" && pswHash != "") {
     ///TODO: Do not save passwords.
 
-    let exists = await storageManager.getUserByEmail(email)
+    let exists = await DBManager.getUserByEmail(email)
     console.log(exists)
 
     if (!exists) {
@@ -75,9 +75,9 @@ USER_API.post("/login", async (req,res) => {
   const {email, pswHash} = req.body;
   console.log(req.body);
 
-  const secretKey = 'mySecretKey';
+  const secretKey =  process.env.SECRET_KEY;;
 
-  const user = await storageManager.getUserByEmailAndPassword(email, pswHash);
+  const user = await DBManager.getUserByEmailAndPassword(email, pswHash);
 
   if(user) {
     const token = jwt.sign({
@@ -98,47 +98,71 @@ USER_API.post("/login", async (req,res) => {
 
 
 //EDIT USER
-
-USER_API.put("/:id", verifyToken, (req, res) => {
-    const userId = parseInt(req.params.id, 10);
-
-    if (!isNaN(userId)) {
-      const foundIndex = users.findIndex((user) => user.id === userId);
+USER_API.put("/updateUser", verifyToken, async (req, res, next) => {
+  const {email, pswHash,name} = req.body
+  const userId = req.user.userId;
   
-      if (foundIndex !== -1) {
-        const { name, email, pswHash } = req.body;
-        users[foundIndex].name = name 
-        users[foundIndex].email = email 
-        users[foundIndex].pswHash = pswHash 
-        res.status(HTTPCodes.SuccessfulResponse.Ok).end();
-        user.save();
-      } else {
-        res.status(HTTPCodes.ClientSideErrorResponse.NotFound).send("User not found").end();
-      }
-    } else {
-      res.status(HTTPCodes.ClientSideErrorResponse.BadRequest).send("Invalid user ID").end();
-    }
-  });
+
+  try {
+    const userUpdate = await DBManager.updateUser(name, email, pswHash, userId);
+    res.status(HTTPCodes.SuccessfulResponse.Ok).json( userUpdate );
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(HTTPCodes.ServerErrorResponse.InternalError).send("Internal Server Error").end();
+  }
+
+
  
-
-
-
-
-USER_API.delete("/:id", verifyToken, (req, res) => {
-    const userId = parseInt(req.params.id, 10);
-
-    if (!isNaN(userId)) {
-      const foundIndex = users.findIndex((user) => user.id === userId);
-  
-      if (foundIndex !== -1) {
-        users.splice(foundIndex, 1);
-        res.status(HTTPCodes.SuccessfulResponse.Ok).end();
-      } else {
-        res.status(HTTPCodes.ClientSideErrorResponse.NotFound).send("User not found").end();
-      }
-    } else {
-      res.status(HTTPCodes.ClientSideErrorResponse.BadRequest).send("Invalid user ID").end();
-    }
 });
 
+ 
+
+USER_API.delete("/deleteUser", verifyToken, async (req, res, next) => {
+  const userId = req.user.userId;
+  try {
+    const userDeletion = await DBManager.deleteUser(userId);
+    res.status(HTTPCodes.SuccessfulResponse.Ok).json( userDeletion );
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(HTTPCodes.ServerErrorResponse.InternalError).send("Internal Server Error").end();
+  }
+});
+
+USER_API.get('/token/', verifyToken, async (req, res) =>{
+  //let userId = await storageManager.getUserById(userId)
+
+const userId = await DBManager.getUserById(id)
+    res.json({ userId });
+})
+
+USER_API.get('/getUserId', verifyToken, (req, res,next) =>{
+  //let userId = await storageManager.getUserById(userId)
+
+const userId = req.user.userId
+//req.params.id
+
+res.json({userId})
+})
+
+
+USER_API.get('/getUserById', verifyToken, async (req, res, next) =>{
+ 
+  const userId = req.user.userId
+  let userInfo = await DBManager.getUserById(userId)
+  
+res.json({userInfo})
+
+  
+})
+////--------------DELETE THIS LATER-----------------
+USER_API.get('/', async (req, res) =>{
+ 
+ 
+  let allUsers = await DBManager.getAllUsers()
+ res.status(200).json(allUsers)
+ return allUsers;
+
+  
+})
+//----------------------------------------------------
 export default USER_API;
--- modules/storageManager.mjs
+++ modules/storageManager.mjs
@@ -1,5 +1,7 @@
 import pg from "pg"
 import SuperLogger from "./superLogger.mjs";
+import jwt from 'jsonwebtoken'
+import userId from "./routes/userRoute.mjs"
 
 // We are using an environment variable to get the db credentials 
 /* if (process.env.DB_CONNECTIONSTRING == undefined) {
@@ -20,41 +22,51 @@ class DBManager {
 
     }
 
-    async updateUser(user) {
+    async updateUser(name,email,pswHash,userId) {
 
         const client = new pg.Client(this.#credentials);
 
         try {
             await client.connect();
-            const output = await client.query('Update "public"."Users" set "name" = $1, "email" = $2, "pswHash" = $3 where id = $4;', [user.name, user.email, user.pswHash, user.id]);
+            const output = await client.query('UPDATE "public"."Users" SET "name" = $1, "email" = $2, "pswHash" = $3 WHERE id = $4 RETURNING *;', 
+            [name, email, pswHash, userId]);
 
             // Client.Query returns an object of type pg.Result (https://node-postgres.com/apis/result)
             // Of special interest is the rows and rowCount properties of this object.
 
             //TODO Did we update the user?
+            return output.rows[0];
 
-        } catch (error) {
-            //TODO : Error handling?? Remember that this is a module seperate from your server 
-        } finally {
-            client.end(); // Always disconnect from the database.
-        }
+            if (output.rows.length > 0) {
+                const updatedUser = output.rows[0];
+                return updatedUser;
+              } else {
+                throw new Error("User not found or not updated");
+              }
+            } catch (error) {
+              console.error("Error updating user:", error);
+              throw error; 
+            } finally {
+              client.end(); // Always disconnect from the database.
+            }
 
         return user;
 
     }
 
-    async deleteUser(user) {
+    async deleteUser(userId) {
 
         const client = new pg.Client(this.#credentials);
 
         try {
             await client.connect();
-            const output = await client.query('Delete from "public"."Users"  where id = $1;', [user.id]);
+            const output = await client.query('Delete from "public"."Users"  where id = $1;', [userId]);
 
             // Client.Query returns an object of type pg.Result (https://node-postgres.com/apis/result)
             // Of special intrest is the rows and rowCount properties of this object.
 
             //TODO: Did the user get deleted?
+            
 
         } catch (error) {
             //TODO : Error handling?? Remember that this is a module seperate from your server 
@@ -62,7 +74,7 @@ class DBManager {
             client.end(); // Always disconnect from the database.
         }
 
-        return user;
+        
     }
 
     async getUserByEmail(email) {
@@ -91,7 +103,7 @@ class DBManager {
             const output = await client.query('INSERT INTO "public"."Users"("name", "email", "pswHash") VALUES($1::Text, $2::Text, $3::Text) RETURNING id;', [user.name, user.email, user.pswHash]);
 
             // Client.Query returns an object of type pg.Result (https://node-postgres.com/apis/result)
-            // Of special intrest is the rows and rowCount properties of this object.
+            // Of special interest is the rows and rowCount properties of this object.
 
             if (output.rows.length == 1) {
                 // We stored the user in the DB.
@@ -126,6 +138,41 @@ class DBManager {
         }
       }
 
+ 
+
+      async getUserById(userId) {
+        const client = new pg.Client(this.#credentials);
+    
+        try {
+            await client.connect();
+    
+            const output = await client.query('SELECT * FROM "public"."Users" WHERE id = $1', [userId]);
+    
+            return output.rows[0];
+        } catch (error) {
+            console.error('Error getting user by ID:', error);
+            throw error;
+        } finally {
+            client.end();
+        }
+    }
+    
+    async getAllUsers() {
+        const client = new pg.Client(this.#credentials);
+    
+        try {
+            await client.connect();
+    
+            const output = await client.query('SELECT * FROM public."Users"');
+    
+            return output.rows;
+        } catch (error) {
+            console.error('cant get all users:', error);
+            throw error;
+        } finally {
+            client.end();
+        }
+    }
 
 }
 
